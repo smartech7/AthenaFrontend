f6193e4593549627ce1083c34da758192779aae4
smartech77 <jonascarlsen77@gmail.com>
2023-08-30 22:45:02 +0300

    Linkedin Updated

--- src/components/widgets/auth/LoginForm.jsx
+++ src/components/widgets/auth/LoginForm.jsx
@@ -141,7 +141,7 @@ export default function LoginForm() {
     clientId: '77jxx0v9fk4l1l',
     redirectUri: 'https://donamix.org/linkedin/callback',
     // redirectUri: `${window.location.origin}/linkedin`, // for Next.js, you can use `${typeof window === 'object' && window.location.origin}/linkedin`
-    scope: 'r_liteprofile r_emailaddress',
+    scope: 'r_emailaddress',
     onSuccess: (code) => {
       console.log(code);
     },
